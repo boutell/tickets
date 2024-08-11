05a083789ca5fb57963d79957bf86124654265ae
boutell <tommybgoode@gmail.com>
2024-08-11 09:42:21 -0400

    the pure defineModel version

--- modules/app/ui/apos/components/Select.vue
+++ modules/app/ui/apos/components/Select.vue
@@ -5,16 +5,10 @@ const props = defineProps({
   choices: Array,
   label: String
 });
-console.log('hi mom');
-const value = ref(model.value || '');
-watch(value, () => {
-  console.log('fired');
-  model.value = value.value;
-});
 </script>
 
 <template>
-  <select v-model="value">
+  <select v-model="model">
     <option
       value=""
     >
