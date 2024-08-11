6c6a75455311489653e98db3f06793f633debb7f
boutell <tommybgoode@gmail.com>
2024-08-11 15:15:04 -0400

    filter logic schematized & behaving with router

--- modules/app/ui/apos/components/Tickets.vue
+++ modules/app/ui/apos/components/Tickets.vue
@@ -6,77 +6,112 @@ const props = defineProps({
   organizationSlug: String
 });
 import { ref, onMounted, watch } from 'vue';
-const organizationChoices = ref([]);
-const assigneeChoices = ref([]);
-const statusChoices = ref([]);
-const organizationSlug = ref('');
-const organization = ref(null);
-const assigneeId = ref('');
-const page = ref(1);
-const status = ref('');
+
 const tickets = ref([]);
-const q = ref('');
 const error = ref(false);
+const loading = ref(true);
 
-onMounted(update);
-watch(organizationSlug, update);
-watch(assigneeId, update);
-watch(status, update);
+onMounted(updateData);
 
-async function update() {
+const filters = [
+  {
+    name: 'organization',
+    choices: ref([]),
+    label: 'Organization'
+  },
+  {
+    name: '_assignee',
+    choices: ref([]),
+    label: 'Assignee'
+  },
+  {
+    name: 'status',
+    choices: ref([]),
+    label: 'Status'
+  },
+  {
+    name: 'page'
+  },
+  {
+    name: 'q'
+  }
+];
+
+for (const filter of filters) {
+  filter.current = ref(route.query[filter.name] || '');
+}
+
+const labeled = filters.filter(filter => !!filter.label);
+
+for (const { name, current } of filters) {
+  watch(current, updateRouteQuery);
+}
+
+function updateRouteQuery() {
+  console.log('in updateRouteQuery');
+  const query = {};
+  for (const { name, current } of filters) {
+    if (current.value) {
+      query[name] = current.value;
+    }
+  }
+  router.push({
+    path: '/',
+    query
+  });
+}
+
+watch(route, updateData);
+
+async function updateData() {
   console.log('in update');
-  const choices = [ 'organization', '_assignee', 'status' ];
+  loading.value = true;
+  const allChoices = [];
   const qs = {
-    choices,
-    page
+    choices: allChoices
   };
-  if (organizationSlug.value) {
-    qs.organization = organizationSlug.value;
+  for (const { name } of filters) {
+    if (route.query[name]) {
+      qs[name] = route.query[name];
+    }
   }
-  if (assigneeId.value) {
-    qs._assignee = assigneeId.value;
-  }
-  if (status.value) {
-    qs.status = status.value;
-  }
-  if (q.value) {
-    qs.q = q.value;
+  for (const { name, choices } of filters) {
+    if (choices) {
+      allChoices.push(name);
+    }
   }
   const response = await apos.http.get('/api/v1/ticket', {
     qs,
     busy: true
   });
   tickets.value = response.results;
-  organizationChoices.value = response.choices.organization;
-  assigneeChoices.value = response.choices._assignee;
-  statusChoices.value = response.choices.status;
+  for (const { name, choices } of filters) {
+    if (choices) {
+      choices.value = response.choices[name];
+    }
+  }
+  loading.value = false;
 }
-
-// watch(() => route.params, () => organizationSlug.value = route.params.organizationSlug);
-// router.push(`/org/${organizationSlug.value}`);
 
 </script>
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink>
   </nav>
-  <section>
-    <label>
-      Organization
-      <Select v-model="organizationSlug" :choices="organizationChoices" />
-    </label>
-    <label>
-      Assignee
-      <Select v-model="assigneeId" :choices="assigneeChoices" />
-    </label>
-    <label>
-      Status
-      <Select v-model="status" :choices="statusChoices" />
-    </label>
+  <section v-if="loading">
+    Loading...
   </section>
-  <section>
-    <ul v-for="ticket in tickets">
-      <li><RouterLink :to="`/ticket/${ticket.slug}`">{{ ticket.title }}</RouterLink></li>
-    </ul>
+  <section v-else>
+    <section>
+      <label v-for="filter in labeled">
+        {{ filter.label }}
+        <Select v-model="filter.current.value" :choices="filter.choices.value" />
+      </label>
+    </section>
+    <section>
+      <ul v-for="ticket in tickets">
+        <li><RouterLink :to="`/ticket/${ticket.slug}`">{{ ticket.title }}</RouterLink></li>
+      </ul>
+    </section>
   </section>
 </template>
